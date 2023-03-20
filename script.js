c3008bf5e8183a8a9f481a0388c34af8cf1c3766
yashraj1309 <dudhatrayashraj@gmail.com>
2023-03-20 16:09:05 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,21 @@
+function validateEmail(emailInputId) {
+  const emailInput = document.getElementById(emailInputId);
+  if(!emailInput) {
+    return;
+  }
+  const email = emailInput.value;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const errorText = document.querySelector(`.${emailInputId}`);
+
+  console.log(email)
+  
+  if (!emailRegex.test(email)) {
+    errorText.style.display = "block";
+    emailInput.style.border = "1px solid red"
+    return false;
+  }
+  
+  errorText.style.display = "none";
+  return true;
+}
